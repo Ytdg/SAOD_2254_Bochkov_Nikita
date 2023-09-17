c0a42b47f52371483a05a9716aabd45c320f8b6e
NikitaBochkov <nikiljgryt@gmail.com>
2023-09-17 15:47:41 +0300

    task-MyQueue

---
+++ Task-Queue/MyQuereTask/MyQuereTask/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyQuereTask
+{
+    public partial class Form1 : Form
+    {
+        MyQueue<int> queu;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            if (Int32.TryParse(tbxCount.Text, out var res))
+            {
+                queu = new MyQueue<int>(res);
+                button1.Enabled=true;
+                button2.Enabled=true;
+                button3.Enabled=true;
+                button4.Enabled=true;
+            }
+            items.Items.Clear();
+            tbxAddItem.Clear();
+            tbxdeleteItem.Clear();
+            tbxGetItem.Clear();
+        }
+        private void UpdateList()
+        {
+            items.Items.Clear();
+            foreach (var s in queu.Values())
+            {
+                items.Items.Add(s);
+            }
+        }
+        private void btnPush_Click(object sender, EventArgs e)
+        {
+            if (Int32.TryParse(tbxAddItem.Text, out var res))
+            {
+                try
+                {
+                    queu.Enqueue(res);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Application.Restart();
+                }
+                UpdateList();
+            }
+
+        }
+
+        private void btnPop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tbxdeleteItem.Text = queu.Dequeue().ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Application.Restart();
+            }
+            UpdateList();
+        }
+
+        private void btnPeek_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tbxGetItem.Text = queu.Peek().ToString();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); Application.Restart(); }
+
+        }
+
+    }
+
+        
+   
+}
---
+++ Task-Queue/MyQuereTask/MyQuereTask/MyQueue.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQuereTask
+{
+    internal class MyQueue<T>
+    {
+        public int Capacity { get; private set; }
+        public int Count { get; private set; }
+        T[] data;
+        T defaultValue;
+        public MyQueue(int size)
+        {
+            Capacity = size;
+            data = new T[Capacity];
+            defaultValue = default(T);
+        }
+        int indexOut;
+        int indexIn;
+        public void Enqueue(T value)
+        {
+            if (Count == Capacity)
+            {
+                throw new Exception("Заполнено");
+            }
+            if (indexIn == Capacity && Count < Capacity)
+            {
+                indexIn = 0;
+            }
+            data[indexIn] = value;
+            indexIn++;
+            Count++;
+        }
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                throw new Exception("Пусто!");
+            }
+
+            return data[indexOut];
+        }
+        public T Dequeue()
+        {
+            if (Count == 0)
+            {
+                throw new Exception("Пусто!");
+            }
+            var value = data[indexOut];
+            data[indexOut] = defaultValue;
+            indexOut++;
+            Count--;
+            if (indexOut == Capacity) { indexOut = 0; }
+            return value;
+        }
+        public T[] Values()
+        {
+            T[] values = new T[Count];
+            int index = 0;
+            if (Count == 0)
+            {
+                indexOut = 0;
+                indexIn = 0;
+            }
+            else
+            {
+                if (indexOut < indexIn)
+                {
+                    for (int i = indexOut; i < indexIn; i++)
+                    {
+                        values[index] = data[i];
+                        index++;
+                    }
+                }
+                else
+                {
+                    for (int i = indexOut; i < Capacity; i++)
+                    {
+                        values[index] = data[i];
+                        index++;
+                    }
+                    for (int i = 0; i < indexIn; i++)
+                    {
+                        values[index] = data[i];
+                        index++;
+                    }
+                }
+            }
+            return values;
+
+        }
+    }
+}
