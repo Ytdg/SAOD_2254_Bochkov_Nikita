dd5d2ac433a73f722993d6cb01a222d87d2a182d
NikitaBochkov <nikiljgryt@gmail.com>
2023-10-08 02:48:01 +0300

    MyVector-Task

---
+++ Task-myVector/MyVecktorTask/MyVecktorTask/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyVecktorTask
+{
+    public partial class Form1 : Form
+    { MyVector<String> vector;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void RefreshItems()
+        {
+            lbx_Items.Items.Clear();
+            foreach(string item in vector) {
+                lbx_Items.Items.Add(item);
+            }
+            lbxCapacity.Text=vector.Capacity.ToString();
+            lbx_Size.Text=vector.Size.ToString();   
+        }
+       private void NoClicBtn(Boolean cheker)
+        {
+            if (cheker)
+            {
+                btnShrinkToFit.Enabled = false;
+                btn_Add.Enabled = false;
+                btn_At.Enabled = false;
+                btn_Create.Enabled = false;
+                btn_Reserve.Enabled = false;
+                btn_Resize.Enabled = false;
+                btn_Set.Enabled = false;
+            }
+            else {
+                btn_Create.Enabled = true;
+            }
+        }
+        private void inputIndInit_ValueChanged(object sender, EventArgs e)
+        {
+            NumericUpDown numericUpDown=sender as NumericUpDown;
+            if(numericUpDown.Value<=0) { NoClicBtn(true); }
+            else { NoClicBtn(false); }
+        }
+
+        private void btn_Create_Click(object sender, EventArgs e)
+        { var valueInit = initValue.Text;
+            if (valueInit.Replace(" ","") == "")
+            {
+                valueInit = "default value";
+
+            }
+            vector = new MyVector<String>((int)inputIndInit.Value, valueInit);
+            btnShrinkToFit.Enabled = true;
+            btn_Add.Enabled = true;
+            btn_At.Enabled = true;
+            btn_Reserve.Enabled = true;
+            btn_Resize.Enabled = true;
+            btn_Set.Enabled = true;
+            RefreshItems();
+            
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            var inputValueAdds = inputValueAdd.Text;
+            if (inputValueAdds.Replace(" ", "") == "")
+            {
+                inputValueAdds = "default value";
+            }
+            vector.Add(inputValueAdds);
+            RefreshItems();
+        }
+
+        private void btn_At_Click(object sender, EventArgs e)
+        {
+            try {
+                ViewValuesIndex.Text = vector[Math.Abs((int)InputIndexGetCalue.Value)];
+            }
+            catch(Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_Set_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                vector[Math.Abs((int)inputIndexSet.Value)] = SetValues.Text.Replace(" ", "") == "" ? "default value" : SetValues.Text;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);    
+            }
+            RefreshItems();
+        }
+
+        
+
+        private void countReserve_ValueChanged(object sender, EventArgs e)
+        {
+            if (countReserve.Value < 0)
+            {
+                countReserve.Value = 0;
+            }
+        }
+
+        private void btn_Resize_Click(object sender, EventArgs e)
+        {
+            var valueInit = initValue.Text;
+            if (valueInit.Replace(" ", "") == "")
+            {
+                valueInit = "default value"; ;
+            }
+            vector.Resize((int)countResize.Value, valueInit);
+            RefreshItems();
+        }
+
+        private void countResize_ValueChanged(object sender, EventArgs e)
+        {
+            if(countResize.Value < 0) { countReserve.Value = 0; }
+        }
+
+        private void btnShrinkToFit_Click(object sender, EventArgs e)
+        {
+            vector.ShrinkToFit();
+            RefreshItems();
+        }
+
+        private void btn_Reserve_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                vector.Reserve((int)countReserve.Value);
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+            RefreshItems();
+        }
+    }
+}
---
+++ Task-myVector/MyVecktorTask/MyVecktorTask/MyVector.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyVecktorTask
+{
+    internal class MyVector<T>:IEnumerable
+    {
+        T[] _data;
+        public int Capacity { get { return _data.Length; } }
+        public int Size { get; private set; }
+        public IEnumerator GetEnumerator()
+        {
+            return new MyVectorEnumerator(_data, Size);
+        }
+        class MyVectorEnumerator : IEnumerator
+        {
+            public object Current { get { return _data[index]; } }
+            T[] _data;
+            int _size;
+            int index = -1;
+            public MyVectorEnumerator(T[] data, int size)
+            {
+                _data = data;
+                _size = size;
+            }
+            public bool MoveNext()
+            {
+                if (index + 1 == _size) return false;
+                index++;
+                return true;
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
+        }
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= Size) throw new Exception("Не корректрный индекс");
+                return _data[index];
+            }
+            set
+            {
+                if (index >= Size) throw new Exception("Не корректрный индекс");
+                _data[index] = value;
+            }
+        }
+        public MyVector(int leng,T initValue)
+        {
+            
+            Size = leng;
+            _data = new T[leng*2];
+            for (int i = 0; i <  leng; i++)
+            {
+                _data[i] = initValue;
+            }
+        }
+        public void Resize(int leng,T defValue)
+        {   
+            if (leng > Capacity)
+            {
+                Reserve(leng * 2);
+            }
+            for (int i = Size; i < leng; i++)
+            {
+                _data[i]=defValue;
+            }
+            Size = leng;
+
+        }
+        public void Reserve(int newcapacity)
+        {
+            if (newcapacity < Size)
+            {
+                throw new Exception("Физическая длина < вектора");
+            }
+            
+            T[] newArray = new T[newcapacity];
+            for (int i = 0; i < Size; i++)
+            {
+                newArray[i] = _data[i];
+            }
+            _data = newArray;
+        }
+        public void SetValue(int index, T value)
+        {
+            if (index >= Size)
+            {
+                throw new Exception("За пределами вектора");
+            }
+            _data[index] = value;
+        }
+        public void ShrinkToFit()
+        {
+            Reserve(Size);
+        }
+        public T At(int index)
+        {
+            if (index >= Size) { throw new Exception("За переделами вектора"); }
+            return _data[index];
+        }
+        public void Add(T value)
+        {
+            if (Size +1 > Capacity)
+            {
+                Reserve((Size * 2)+2);
+                _data[Size] = value;
+            }
+            _data[Size] = value;
+
+                    
+            Size++;
+        }
+
+    }
+}
